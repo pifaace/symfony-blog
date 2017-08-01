be59517922c9219e39b56413b9557756639eb2c5
pifaace <maximejoassy@hotmail.fr>
2017-08-01 16:22:40 +0200

    :package: Add Comment Entity

---
+++ src/AppBundle/Entity/Comment.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Advert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comment")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CommentRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Comment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="createAt", type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Advert", inversedBy="comments")
+     * @ORM\JoinColumn(name="advert_id", nullable=false)
+     */
+    private $advert;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Comment
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set createAt
+     * @ORM\PrePersist
+     */
+    public function setCreateAt()
+    {
+        $this->createAt = (new \DateTime());
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return string
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * Set author
+     *
+     * @param User $author
+     *
+     * @return Comment
+     */
+    public function setAuthor(User $author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return Advert
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set advert
+     *
+     * @param Advert $advert
+     *
+     * @return Comment
+     */
+    public function setAdvert(Advert $advert)
+    {
+        $this->advert = $advert;
+
+        return $this;
+    }
+
+    /**
+     * Get advert
+     *
+     * @return Advert
+     */
+    public function getAdvert()
+    {
+        return $this->advert;
+    }
+}
