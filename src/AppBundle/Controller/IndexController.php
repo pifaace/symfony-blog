04d302ca2bbf720bb25bf3ddf2a8e8b7b4345cf5
JOASSY Maxime <maxime.joassy@sully-group.fr>
2017-08-01 10:19:02 +0200

    :package: Add NPM - Gulp

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -10,8 +10,9 @@ class IndexController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         return $this->render('home/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
