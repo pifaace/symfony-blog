656e76b4eb6ff4d2d9f3faaf0aa7382ae618cfff
pifaace <maximejoassy@hotmail.fr>
2017-08-02 17:08:08 +0200

    :package: Add Badge and UnblockBadge Entity

---
+++ src/AppBundle/Entity/Badge.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Badge
+ *
+ * @ORM\Table(name="badge")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BadgeRepository")
+ */
+class Badge
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="actionName", type="string", length=255)
+     */
+    private $actionName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Badge
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Badge
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set actionName
+     *
+     * @param string $actionName
+     *
+     * @return Badge
+     */
+    public function setActionName($actionName)
+    {
+        $this->actionName = $actionName;
+
+        return $this;
+    }
+
+    /**
+     * Get actionName
+     *
+     * @return string
+     */
+    public function getActionName()
+    {
+        return $this->actionName;
+    }
+}
+
