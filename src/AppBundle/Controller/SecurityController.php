28b508bf5219811b5fd0ee2223991e9f4b64dc00
piface <maximejoassy@hotmail.fr>
2017-09-09 18:00:03 +0200

    :package: Adding redirect after login. Adding logout route

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -14,19 +13,21 @@ class SecurityController extends Controller
     /**
      * @Route("/login", name="login")
      *
-     * @param Request $request
      * @param AuthenticationUtils $authUtils
      * @return Response
      */
-    public function loginAction(Request $request, AuthenticationUtils $authUtils)
+    public function loginAction(AuthenticationUtils $authUtils): Response
     {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('admin-dashboard');
+        }
+
         $error = $authUtils->getLastAuthenticationError();
         $lastUsername = $authUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', array(
-           'lastUsername' => $lastUsername,
+            'lastUsername' => $lastUsername,
             'error' => $error
         ));
-
     }
 }
