724ade6d65ee8d2bd8283d05a26196155a6c2931
piface <maximejoassy@hotmail.fr>
2017-09-24 23:01:38 +0200

    :package: Adding delete confirmation

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
 
-gulp.task('default', ['sass', 'fonts'], function () {
+gulp.task('default', ['sass', 'fonts', 'js'], function () {
 });
 
 gulp.task('sass', function () {
     return gulp.src('assets/scss/app.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('web/build/css/app.css'));
+        .pipe(gulp.dest('web/build/css'));
 });
 
 gulp.task('sass:watch', function () {
@@ -17,4 +18,10 @@ gulp.task('sass:watch', function () {
 gulp.task('fonts', function () {
    gulp.src('node_modules/font-awesome/fonts/**')
        .pipe(gulp.dest('web/build/fonts'))
+});
+
+gulp.task('js', function () {
+    gulp.src('assets/js/app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('web/build/js'))
 });
