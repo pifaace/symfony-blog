985dd9a625b44dbd9a243b95ad1726ac993898ba
piface <maximejoassy@hotmail.fr>
2018-01-07 23:29:14 +0100

    :package: Add TagTransform transformTest

--- tests/Unit/Form/DataTransformer/TagsTransformerTest.php
+++ tests/Unit/Form/DataTransformer/TagsTransformerTest.php
@@ -1,10 +1,44 @@
 <?php
 
-namespace test\Unit\Form\DataTransformer;
+namespace App\Tests\Unit\Form\DataTransformer;
 
 use App\Entity\Tag;
+use App\Form\DataTransformer\TagsTransformer;
+use Doctrine\Common\Persistence\ObjectManager;
 use PHPUnit\Framework\TestCase;
 
 class TagsTransformerTest extends TestCase
 {
+    public function testTransform()
+    {
+        $tagsArray = [
+          $this->createTag('Symfony'),
+          $this->createTag('Test'),
+          $this->createTag('Unit'),
+        ];
+
+        $transformer = $this->getMockedTransformer();
+        $tagsTransformed = $transformer->transform($tagsArray);
+
+        $this->assertEquals('Symfony, Test, Unit', $tagsTransformed);
+    }
+
+    private function getMockedTransformer()
+    {
+        $entityManager = $this
+            ->getMockBuilder(ObjectManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return new TagsTransformer($entityManager);
+    }
+
+    private function createTag($name)
+    {
+        $tag = new Tag();
+        $tag->setName($name);
+
+        return $tag;
+    }
+
 }
