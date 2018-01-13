72687c194e253677c3d8895c72177cae1b7dca41
Maxime JOASSY <maximejoassy@hotmail.fr>
2018-01-13 18:03:17 +0100

    :package: Add Paginator Unit test

---
+++ tests/Unit/Services/PaginatorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Unit\Services;
+
+use App\Entity\Article;
+use App\Services\Paginator;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class PaginatorTest extends TestCase
+{
+    public function testCountPage()
+    {
+        $articleArray = [
+            $this->createArticle(),
+            $this->createArticle(),
+            $this->createArticle(),
+            $this->createArticle(),
+            $this->createArticle(),
+            $this->createArticle()
+        ];
+
+        $this->assertEquals($this->getMockedPaginator()->countPage($articleArray), '2');
+    }
+
+    private function getMockedPaginator()
+    {
+        $requestStack = $this
+            ->getMockBuilder(RequestStack::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return new Paginator($requestStack, '5');
+    }
+
+    private function createArticle()
+    {
+        $article = new Article();
+
+        return $article;
+    }
+}
