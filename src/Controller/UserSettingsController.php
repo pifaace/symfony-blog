8d3ebfb9ad8a8f1d764369429f4bbb9b8f2a4563
Maxime JOASSY <maximejoassy@hotmail.fr>
2018-05-17 20:09:05 +0200

    :package: Add sidebar user settings

--- src/Controller/UserSettingsController.php
+++ src/Controller/UserSettingsController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends Controller
+/**
+ * @Route("/user")
+ */
+class UserSettingsController extends Controller
 {
     /**
-     * @Route("/user/profile", name="user_profile")
+     * @Route("/profile", name="user_profile")
      */
-    public function index()
+    public function show()
     {
         return $this->render('blog/user/profile/show.html.twig');
     }
