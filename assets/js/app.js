31d33240b34939fed04cc89ab76c7b7d57a25105
Maxime JOASSY <maximejoassy@hotmail.fr>
2018-11-19 23:27:27 +0100

    build(Dependencies): remove symfony/lts abandonned

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-window.$ = window.jQuery = require('jquery');
-require('./delete-confirmation');
-require('./tags');
-require('./input-file');
-require('./delete-notification');
-require('./button-click-back');
