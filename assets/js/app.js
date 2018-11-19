66496ddc29d96df2f003abe57e6d3c5e340fb215
Maxime JOASSY <maximejoassy@hotmail.fr>
2018-11-19 23:42:33 +0100

    build(Dependencies): add webpack-encore

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+window.$ = window.jQuery = require('jquery');
+require('./delete-confirmation');
+require('./tags');
+require('./input-file');
+require('./delete-notification');
+require('./button-click-back');
--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
